5e5d2598195a5bef3b7e8b2b947966b913ddc3a2
petercheng168 <31024962+petercheng168@users.noreply.github.com>
2017-09-01 00:49:11 +0800

    上傳測試

--- C#-WD-kkbox-dislike.cs
+++ C#-WD-kkbox-dislike.cs
@@ -3,48 +3,94 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
-using Selenium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
 {
-[TestFixture]
-public class dislike
-{
-private ISelenium selenium;
-private StringBuilder verificationErrors;
-
-[SetUp]
-public void SetupTest()
-{
-selenium = new DefaultSelenium("localhost", 4444, "*chrome", "https://play.kkbox.com/");
-selenium.Start();
-verificationErrors = new StringBuilder();
-}
-
-[TearDown]
-public void TeardownTest()
-{
-try
-{
-selenium.Stop();
-}
-catch (Exception)
-{
-// Ignore errors if unable to close the browser
-}
-Assert.AreEqual("", verificationErrors.ToString());
-}
-
-[Test]
-public void TheDislikeTest()
-{
-			selenium.Open("https://play.kkbox.com/");
-			selenium.Type("name=nickName", "0933751972");
-			selenium.Type("name=password", "peter528");
-			selenium.Click("id=login-btn");
-			selenium.Click("link=電台");
-			selenium.Click("css=a.btn-radio");
-			selenium.Click("//div[@id='player']/div[6]/a/i");
-}
-}
+    [TestFixture]
+    public class Dislike
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://play.kkbox.com/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheDislikeTest()
+        {
+            driver.Navigate().GoToUrl("https://play.kkbox.com/");
+            driver.FindElement(By.Name("nickName")).Clear();
+            driver.FindElement(By.Name("nickName")).SendKeys("0933751972");
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys("peter528");
+            driver.FindElement(By.Id("login-btn")).Click();
+            driver.FindElement(By.LinkText("電台")).Click();
+            driver.FindElement(By.CssSelector("a.btn-radio")).Click();
+            driver.FindElement(By.XPath("//div[@id='player']/div[6]/a/i")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
 }
